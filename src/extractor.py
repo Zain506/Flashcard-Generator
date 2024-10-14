c66386a2180e25ee42b8aa2f2975bf0973682745
= <nomani.zain@gmail.com>
2024-10-14 14:12:49 +0100

    Tidy code inputs for LLM

--- src/extractor.py
+++ src/extractor.py
@@ -15,8 +15,7 @@ class Extractor:
     def __init__(self, folder: str):
         self.folder = folder
     def run(self):
-        ex: list = self._load_text()
-        sys: str = self._makeQuery(ex)
+        sys: str = self._createPrompt()
         num: int = self._getLenChunks()
         for i in range(num):
             text: str = self._readChunk(i)
@@ -30,53 +29,33 @@ class Extractor:
         with open(f"experiments/{self.folder}/examples/extracted_text.txt", "r", encoding = "utf-8") as file:
             extract = file.read()
         return [example, extract]
-    def _makeQuery(self, list) -> str:
+    def _createPrompt(self) -> str:
         """Use list of 2 texts to form system message"""
         # Search to find prompt.txt - if not there, then return the standard examples prompt
-        directory: str = "examples"
+        directory: str = f"experiments/{self.folder}/examples"
         file_path = os.path.join(directory, 'prompt.txt')
         # prompt.txt must ask for a JSON-formatted response
-        if os.path.exists(file_path):
-            with open(file_path, 'r') as file:
-                content = file.read()
-            return content
-        content = f"""
-Here is an example of some text in which definitions, lemmas, theorems, propositions and corollaries are extracted:
+        with open(file_path, 'r') as file:
+            content = file.read()
+            if content:
+                return content
+        content = """
+        Using the provided information in the user message, create flashcards for a student trying to learn this topic.
 
-{list[0]}
-
-And here is the extracted result in json format:
-
-{list[1]}
-
-You will use the examples given above to perform the same task on the text provided in the user message.
-Ignore exercises, solutions, examples and proofs. Do NOT include any exercises, solutions, examples or proofs.
-You will NEVER return a nested JSON. Every key-value pair must be a string.
-
-Do NOT do this:
-{{
-    "key1": {{
-        ...
-    }},...
-}}
-
-You will always make each value a string. No other data types. I.e.:
-
-{{
-    "key1": "value1",
-    "key2": "value2",
-    ...
-}}
-"""
+        Provide the solution in JSON format, where the front of the card is the key (i.e. the question) and the back is the value (i.e. the answer)
+        """
         return content
+    
     def _getLenChunks(self) -> int:
         """Return how many times to read chunks"""
         return len(os.listdir(f"experiments/{self.folder}/chunks/"))
+    
     def _readChunk(self, chunk: int) -> str:
         """Read in chunk of text"""
         with open(f"experiments/{self.folder}/chunks/chunk{chunk}.txt", "r", encoding = "utf-8") as file:
             content = file.read()
             return content
+    
     def _queryLang(self, sys: str, user: str) -> str:
         """Query Langchain and return json of response"""
         # print(sys)
@@ -95,14 +74,17 @@ You will always make each value a string. No other data types. I.e.:
         chain = prompt|llm # combines both
         response = chain.invoke({"input": usermd})
         return response.content
+    
     def _parseText(self, myDict: str) -> dict:
         """Where the text values contain escape characters, remove and fix"""
         return myDict.replace("\\\\", "\\").replace("\(", "$$").replace(")\\", "$$")
+    
     def _storeText(self, text: str, filename: str) -> None:
         """Temporarily store as text"""
         os.makedirs(f"experiments/{self.folder}/output/", exist_ok = True)
         with open(f"experiments/{self.folder}/output/{filename}", "w") as file:
             file.write(text)
+    
     def _storeQuery(self, dictionary: str, filename:str) -> None:
         """Store langchain response as .json"""
         os.makedirs(f"experiments/{self.folder}/output/", exist_ok = True)
